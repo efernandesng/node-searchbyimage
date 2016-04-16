1a517818c341de22b031c2582de9824148d5967c
Emanuel Fernandes <efernandes@tektorque.com>
2016-04-16 21:36:30 +0100

    replace superagent by request

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
-import validUrl from 'valid-url';
+import fs from 'fs';
 import Promise from 'bluebird';
-import request from 'superagent';
+import {isUri} from 'valid-url';
+import request from 'request';
 import cheerio from 'cheerio';
 
 const BASE_URL = 'https://www.google.com/searchbyimage';
 
 const searchByImage = (image, opts, cb)=> {
-  const isUrl = (typeof image === 'string' && validUrl(image));
   if (typeof opts === 'function') cb = opts;
 
-  let req;
-  if (isUrl) {
-    req = request
-      .get(BASE_URL)
-      .query({
+  return new Promise((resolve, reject)=> {
+    const reqOpts = (typeof image === 'string' && isUri(image)) ? {
+      method: 'GET',
+      url: BASE_URL,
+      qs: {
         site: 'search',
         sa: 'X',
         image_url: image
-      })
-  } else {
-    req = request
-      .post(BASE_URL + '/uplaod')
-      .attach('encoded_image', image);
-  }
+      }
+    } : {
+      method: 'POST',
+      url: BASE_URL + '/uplaod',
+      formData: {
+        encoded_image: (image instanceof Buffer) ? image : fs.createReadStream(image)
+      }
+    };
 
-  return new Promise((resolve, reject)=> {
-    req.set({
-      'User-Agent': 'Mozilla/5.0',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-    }).end((err, res)=> {
+    request({
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+      },
+      ...reqOpts
+    }, (err, res, body)=> {
       if (err) return reject(err);
 
-      const $ = cheerio.load(res.text);
+      const $ = cheerio.load(body);
       const guess = $('a._gUb').text();
 
       resolve({guess})
