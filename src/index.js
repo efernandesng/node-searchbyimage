0bcfcb8c21b2ae096f3e97e8665191d991abde83
Emanuel Fernandes <efernandes@tektorque.com>
2016-04-16 22:02:09 +0100

    add options

--- src/index.js
+++ src/index.js
@@ -5,9 +5,28 @@ import request from 'request';
 import cheerio from 'cheerio';
 
 const BASE_URL = 'https://www.google.com/searchbyimage';
+const CSS_QUERY = 'div#topstuff div.card-section > div:nth-child(3) > a';
+
+const defOptions = {
+  userAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
+  acceptLanguage: 'en-US,en;q=0.5'
+};
 
 const searchByImage = (image, opts, cb)=> {
-  if (typeof opts === 'function') cb = opts;
+  const typeOfOpts = typeof opts;
+
+  let options = defOptions;
+  switch (typeOfOpts) {
+    case 'object':
+      options = {
+        ...defOptions,
+        ...opts
+      };
+      break;
+    case 'function':
+      cb = opts;
+      break;
+  }
 
   return new Promise((resolve, reject)=> {
     const reqOpts = (typeof image === 'string' && isUri(image)) ? {
@@ -28,15 +47,16 @@ const searchByImage = (image, opts, cb)=> {
 
     request({
       headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+        'User-Agent': options.userAgent,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': options.acceptLanguage
       },
       ...reqOpts
     }, (err, res, body)=> {
       if (err) return reject(err);
 
       const $ = cheerio.load(body);
-      const guess = $('a._gUb').text();
+      const guess = $(CSS_QUERY).text();
 
       resolve({guess})
     })
