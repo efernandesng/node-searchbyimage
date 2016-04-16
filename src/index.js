56f82a77ac1cbbef0961b908cb5bd010d899f7b4
Emanuel Fernandes <efernandes@tektorque.com>
2016-04-16 19:09:51 +0100

    first implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
+import validUrl from 'valid-url';
+import Promise from 'bluebird';
 import request from 'superagent';
+import cheerio from 'cheerio';
 
-module.exports = (image, opts) => {
-  console.log(image, opts);
+const BASE_URL = 'https://www.google.com/searchbyimage';
+
+const searchByImage = (image, opts, cb)=> {
+  const isUrl = (typeof image === 'string' && validUrl(image));
+  if (typeof opts === 'function') cb = opts;
+
+  let req;
+  if (isUrl) {
+    req = request
+      .get(BASE_URL)
+      .query({
+        site: 'search',
+        sa: 'X',
+        image_url: image
+      })
+  } else {
+    req = request
+      .post(BASE_URL + '/uplaod')
+      .attach('encoded_image', image);
+  }
+
+  return new Promise((resolve, reject)=> {
+    req.set({
+      'User-Agent': 'Mozilla/5.0',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }).end((err, res)=> {
+      if (err) return reject(err);
+
+      const $ = cheerio.load(res.text);
+      const guess = $('a._gUb').text();
+
+      resolve({guess})
+    })
+  }).nodeify(cb);
 };
+
+module.exports = searchByImage;
